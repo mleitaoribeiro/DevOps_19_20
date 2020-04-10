44c69a79cb73eb14838a3e4439b402c1ec1aa9c4
Marta Ribeiro <1191779@isep.ipp.pt>
2020-04-10 23:10:38 +0100

    fix #25 and fix #26

---
+++ ca2/Alternativa Ant/Part1/backup/test/java/basic_demo/AppTest.java
@@ -0,0 +1,14 @@
+package basic_demo;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+
+    @Test
+    public void testAppHasAGreeting() {
+        final App classUnderTest = new App();
+        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    }
+
+}
